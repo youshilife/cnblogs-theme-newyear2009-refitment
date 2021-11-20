c295dd314c36e13c2b29c0feaa0812a61bb6fa0d
幽室星辰 <youshilife@qq.com>
2021-11-20 23:33:28 +0800

    处理文章详情

--- scripts/_dom.js
+++ scripts/_dom.js
@@ -18,10 +18,24 @@ function setAccessMode() {
 }
 
 /**
- * ====================================
- * 调整内容
- * ====================================
+ * 移除DOM结点
+ *
+ * @params {NodeList} nodes 结点列表
  */
+function removeNodes(nodes) {
+    for (let node of nodes) {
+        node.remove();
+    }
+}
+
+/**
+ * 处理链接。
+ */
+function handleLinks() {
+    // 举报
+    let links = document.querySelectorAll("a[onclick*='reportManager.report']");
+    removeNodes(links);
+}
 
 /**
  * 处理导航栏。
@@ -57,7 +71,6 @@ function handleDayList() {
     /**
      * 处理文章摘要
      */
-
     let summaries = document.querySelectorAll(".day .c_b_p_desc");
     for (let elem of summaries) {
         let html = elem.innerHTML.trim();
@@ -66,7 +79,6 @@ function handleDayList() {
         // 包裹“阅读全文”
         html = `${html.substring(0, html.indexOf("<"))}……
 <div class="x-read-more">${html.substring(html.indexOf("<"))}</div>`;
-
         elem.innerHTML = html;
     }
 
@@ -83,11 +95,43 @@ function handleDayList() {
         // 包裹时间
         let time = html.substring(0, html.indexOf("<"));
         html = `<span class="x-post-time">${time}</span>${html.substring(html.indexOf("<"))}`;
-
         elem.innerHTML = html;
     }
 }
 
+/**
+ * 处理文章详情。
+ */
+function handlePost() {
+    let postDetail = document.querySelector("#post_detail");
+
+    /**
+     * 处理文章信息内容。
+     */
+    let postDesc = postDetail.querySelector(".postDesc");
+    let html = postDesc.innerHTML.trim();
+    // 移除“posted”
+    html = html.replace("posted @", "");
+    // 替换“&nbsp;”为空格
+    html = html.replaceAll("&nbsp;", " ");
+    // 包裹发布时间和作者
+    html = `<div class="x-post-time-author">${html.substring(0, html.indexOf("</a>") + "</a>".length)}</div>
+${html.substring(html.indexOf("</a>") + "</a>".length)}`;
+    postDesc.innerHTML = html;
+
+    /**
+     * 调整DOM结构
+     */
+    // 将文章信息元素移动到紧跟正文之后
+    let postBody = postDetail.querySelector("#cnblogs_post_body");
+    postBody.parentElement.insertBefore(postDesc, postBody.nextSibling);
+    // 将分类和标签元素移动到文章信息元素中
+
+}
+
 setAccessMode();
+
+handleLinks();
 handleNavbar();
 handleDayList();
+handlePost();
