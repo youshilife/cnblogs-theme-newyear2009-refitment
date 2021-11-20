99db98c3980719c2955d3274837f02f22d25e19b
幽室星辰 <youshilife@qq.com>
2021-11-20 20:19:42 +0800

    添加百度统计功能

---
+++ scripts/_stats.js
@@ -0,0 +1,30 @@
+/**
+ * ============================================================================
+ * 数据统计
+ * ============================================================================
+ */
+
+// 百度统计令牌
+const BAIDU_TONGJI_TOKEN = "a0df9b96f6d9c1ce00e5eb77a70907a7";
+
+/**
+ * 加载百度统计
+ *
+ * @param {string} token 令牌
+ */
+function loadBaiduTongji(token) {
+    console.debug("加载百度统计");
+    var _hmt = _hmt || [];
+    (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?" + token;
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+    })();
+}
+
+// 如果访问者不是博主，则加载统计组件
+if (!window.isBlogOwner) {
+    console.debug("加载统计组件");
+    loadBaiduTongji(BAIDU_TONGJI_TOKEN);
+}
